a28be6dd608bc3f589eef5a23083f3a0568c0c39
Michael Harris <mhguitar1997@gmail.com>
2019-08-08 14:58:54 -0700

    Create baseball athlete

---
+++ athlete/src/com/lambdaschool/solution/BaseballAthlete.java
@@ -0,0 +1,11 @@
+package src.com.lambdaschool.solution;
+
+public class BaseballAthlete implements AthleteCreation {
+    @Override
+    public void displayAthlete() {
+
+        System.out.println("************");
+        System.out.println("Baseball Athlete");
+        System.out.println("************");
+    }
+}
