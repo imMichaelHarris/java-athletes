7c4f7e7f5187ac33c892bfccd75416b786be713c
Michael Harris <mhguitar1997@gmail.com>
2019-08-08 14:49:39 -0700

    Track runner in

--- athlete/src/com/lambdaschool/solution/TrackRunner.java
+++ athlete/src/com/lambdaschool/solution/TrackRunner.java
@@ -1,5 +1,12 @@
 package src.com.lambdaschool.solution;
 
-public class TrackRunner implements A {
+public class TrackRunner implements AthleteCreation {
+    @Override
+    public void displayAthlete() {
 
+        System.out.println("************");
+        System.out.println("Track Runner");
+        System.out.println("************");
+
+    }
 }
--- athlete/src/com/lambdaschool/solution/TrackRunnerInjector.java
+++ athlete/src/com/lambdaschool/solution/TrackRunnerInjector.java
@@ -3,6 +3,6 @@ package src.com.lambdaschool.solution;
 public class TrackRunnerInjector implements AthleteCreationInjector {
     @Override
     public Processor getProcess(){
-        return new MyApplication(new TrackRunner());
+        return new MyApplication(new TrackRunner(TrackRunnerImpl));
     }
 }
