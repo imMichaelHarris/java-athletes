ae8d15c6de44f75feec3c122ed9ca0f51a604dfd
Michael Harris <mhguitar1997@gmail.com>
2019-08-08 14:46:24 -0700

    My app class completed with display override

--- athlete/src/com/lambdaschool/solution/MyApp.java
+++ athlete/src/com/lambdaschool/solution/MyApp.java
@@ -1,4 +1,14 @@
 package src.com.lambdaschool.solution;
 
-public class MyApp {
+public class MyApp implements Processor {
+    private AthleteCreation myAthlete;
+
+    public MyApp(AthleteCreation myAthlete) {
+        this.myAthlete = myAthlete;
+    }
+
+    @Override
+    public void displayAthlete() {
+        myAthlete.displayAthlete();
+    }
 }
