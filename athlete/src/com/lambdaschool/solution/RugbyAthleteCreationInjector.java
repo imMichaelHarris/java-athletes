d5fe4438dd70976c255ac16856cc9541cef9623a
Michael Harris <mhguitar1997@gmail.com>
2019-08-08 15:12:33 -0700

    rugby athlete gets properly called

--- athlete/src/com/lambdaschool/solution/RugbyAthleteCreationInjector.java
+++ athlete/src/com/lambdaschool/solution/RugbyAthleteCreationInjector.java
@@ -3,6 +3,6 @@ package src.com.lambdaschool.solution;
 public class RugbyAthleteCreationInjector implements AthleteCreationInjector {
     @Override
     public Processor getProcess() {
-        return new MyApp(new BaseballAthlete());
+        return new MyApp(new RugbyAthlete());
     }
 }
